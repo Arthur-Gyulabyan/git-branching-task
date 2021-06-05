2ac469c849e16e9974fc70178027060830f1ccc1
Arthur <gyulabyana@gmail.com>
2021-06-05 18:33:47 +0400

    feat: "deepCopy()" function created

---
+++ pr.js
@@ -0,0 +1,34 @@
+// Deep copy
+
+const deepCopy = (obj) => {
+    let objCopy = {};
+
+    for (const key in obj) {
+        const curValue = obj[key];
+
+        if (typeof curValue === 'object' && curValue) {
+            objCopy[key] = deepCopy(curValue);
+        } else {
+            objCopy[key] = curValue;
+        }
+    }
+
+    return objCopy;
+};
+
+const obj = {
+    fullName: {
+        name: 'aaa',
+        lastName: 'bbbb',
+        aaa: {
+            lll: 'asd',
+            jjj: 12,
+        }
+
+    },
+
+    age: 20,
+};
+
+const objCopy = deepCopy(obj);
+console.log(objCopy === obj); // false
