093865adda20acb7467b2fb3dd41e778d298e5ae
Arthur <gyulabyana@gmail.com>
2021-06-06 23:28:35 +0400

    feat: user class created

--- users.js
+++ users.js
@@ -1,5 +1,20 @@
-const user = {
-    fnmae: 'Arthur',
-    lname: 'Gyulabyan',
-    age: 22,
-};
+class User {
+    constructor(fname, lname, birthYear) {
+        this.fname = fname;
+        this.lname = lname;
+        this.birthYear = birthYear;
+    }
+
+    greeting() {
+        return `Hi ${this.fname}.`;
+    }
+
+    calcAge() {
+        return new Date().getFullYear() - this.birthYear;
+    }
+}
+
+const arthur = new User('Arthur', 'Gyulabyan', 1998);
+
+console.log(arthur.greeting());
+console.log(arthur.calcAge());
