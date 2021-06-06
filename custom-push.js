8a47fdb0fcd9f77189d000327585b9c706b7be15
Arthur <gyulabyana@gmail.com>
2021-06-06 14:19:25 +0400

    fix: custom-push.js - bugs fixed

--- custom-push.js
+++ custom-push.js
@@ -1,8 +1,8 @@
 // Implementation of Array.prototype.push()
 
 Array.prototype.customPush = function (...item) {
-    for (let i = 0; i < this.length; i++) {
-        this.length = item[i];
+    for (let i = 0; i < item.length; i++) {
+        this[this.length] = item[i];
     }
 
     return this.length;
@@ -10,7 +10,7 @@ Array.prototype.customPush = function (...item) {
 
 const numbers = [1, 2, 3];
 
-numbers.push(4);
-console.log(numbers.push(5, 6)); // 6
+numbers.customPush(4);
+console.log(numbers.customPush(5, 6)); // 6
 
 console.log(numbers); // [1, 2, 3, 4, 5, 6]
